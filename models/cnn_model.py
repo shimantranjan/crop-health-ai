a0e8ea47675d2da841e48b8b721ac195aae24480
shimantranjan <shimantranjan2@gmail.com>
2026-02-05 18:23:43 +0530

    Initial clean commit: crop health AI project

---
+++ models/cnn_model.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def build_cnn_model(num_classes):
+    model = models.Sequential([
+        layers.Input(shape=(224, 224, 3)),
+
+        layers.Conv2D(32, (3, 3), activation='relu'),
+        layers.MaxPooling2D(2, 2),
+
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D(2, 2),
+
+        layers.Conv2D(128, (3, 3), activation='relu'),
+        layers.MaxPooling2D(2, 2),
+
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+
+    model.compile(
+        optimizer='adam',
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    return model
